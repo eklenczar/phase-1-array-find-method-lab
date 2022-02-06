e1d31d09e3ab26e34c7ed42e6ab66ebd39d97f1f
Elizabeth Klenczar <eklenczar@gmail.com>
2022-02-06 17:01:07 -0500

    commit tree 6d7cf4ab1b6bae476c7e2bdd9c7c4a4e5630549e

--- index.js
+++ index.js
@@ -1,9 +1,3 @@
-const record = [
-    { year: "2015", result: "W"},
-    { year: "2014", result: "N/A"},
-    { year: "2013", result: "L"},
-  ]
-
 function superbowlWin(records) {
     const found = records.find((record) => {
         return (record.result === 'W') 
@@ -13,5 +7,3 @@ function superbowlWin(records) {
     }    
         console.log('found', found)
     }
-
-console.log(superbowlWin(record))
