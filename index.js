f4239a3d2ed3597d78949a3a44407bf853021f2d
Elizabeth Klenczar <eklenczar@gmail.com>
2022-02-06 15:50:58 -0500

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// code your solution here
+const record = [
+    { year: "2015", result: "W"},
+    { year: "2014", result: "N/A"},
+    { year: "2013", result: "L"},
+  ]
+
+function superbowlWin(record) {
+    for(const win of record) {
+        if(win.result === 'W') {
+            return win.year
+        }
+    }
+}
+
+
